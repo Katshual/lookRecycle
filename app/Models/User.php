9f85901cc48b034e7985e1e1393dd7148c68e611
Katshual <139873320+Katshual@users.noreply.github.com>
2025-03-31 17:57:41 +0200

    installation de laravel sanctum pour les routes API et mise à jour de la configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
     /**
      * Les attributs modifiables en masse.
      * Permet de définir les champs qui peuvent être assignés lors de la création ou mise à jour d'un utilisateur.
